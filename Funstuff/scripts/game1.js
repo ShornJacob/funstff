30f5700d34f6f7ab509d766c883d07137e1bcf02
Shorn Jacob <shorn.jacob1986@gmail.com>
2016-11-08 18:05:57 +1300

    simple canvas

--- Funstuff/scripts/game1.js
+++ Funstuff/scripts/game1.js
@@ -1,13 +1,6 @@
-﻿var myGameArea = {
-    canvas: document.createElement("canvas"),
-    start: function () {
-        this.canvas.width = 480;
-        this.canvas.height = 270;
-        this.context = this.canvas.getContext("2d");
-        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-    }
+﻿var draw = function () {
+    var canvas = document.getElementById("tutorial");
+    var ctx = canvas.getContext("2d");
+
 }
 
-function startGame() {
-    myGameArea.start();
-}
