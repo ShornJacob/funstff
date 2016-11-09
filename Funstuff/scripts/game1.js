b2eb900db4fed30983b9680dedfc0ec3110378ec
Shorn Jacob <shorn.jacob1986@gmail.com>
2016-11-09 17:56:59 +1300

    jquery hide

--- Funstuff/scripts/game1.js
+++ Funstuff/scripts/game1.js
@@ -1,6 +1,15 @@
-﻿var draw = function () {
-    var canvas = document.getElementById("tutorial");
-    var ctx = canvas.getContext("2d");
+﻿$(document).ready(function () {
+    $("button").click(function () {
+        $("p").hide();
+    })
+});
 
-}
+
+//var draw = function () {
+//    var canvas = document.getElementById("tutorial");
+//    var ctx = canvas.getContext("2d");
+
+//    ctx.fillStyle = "green";
+//    ctx.fillRect(10, 10, 100, 100);
+//}
 
