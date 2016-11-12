d090bef9a92ac86e46a3c06aefde7433a6606e23
shorn <shorn.jacob1986@gmail.com>
2016-11-12 15:28:02 +1300

    css, move a box

--- Funstuff/scripts/game1.js
+++ Funstuff/scripts/game1.js
@@ -1,14 +1,62 @@
 ﻿$(document).ready(function () {
-    $("button").click(function () {
-        $("#paraid").hide();
+    $("#but1").click(function () {
+        //$("#paraid").toggle(1000);
+        //$("#div1").slideToggle();
+        //$("#div2").fadeToggle("slow");
+        //$("#div3").fadeToggle(3000);
 
+        $("#element1").animate({left: '+=250px'});
+        $("#element1").animate({top: '+=250px'});
+        $("#element1").animate({left: '-=250px'});
+        $("#element1").animate({top: '-=250px'});   
+    })
+
+
+
+    $("#but2").click(function () {
+       
+        $("#div1").fadeTo("slow",0.15);
+        $("#div2").fadeTo("slow", 0.4);
+        $("#div3").fadeTo("slow", 0.7);
     });
 
-    $("p").click(function () {
-        $(this).hide();
+    $("p").on({
+        mouseenter: function () {
+            $(this).css("background-color", "lightgray");
+        },
+
+        mouseleave: function () {
+            $(this).css("background-color", "lightblue");
+        },
+
+        click: function () {
+            $(this).css("background-color", "yellow")
+        }
     });
+
+    $("input").on({
+        focus: function () {
+            $(this).css("background-color", "#cccccc");
+        },
+
+        blur: function () {
+            $(this).css("background-color", "#ffffff");
+        }
+
+    
+    });
+
 
 });
+
+   
+
+//$("p").click(function () {
+//    $(this).hide();
+//});
+
+
+
 //var draw = function () {
 //    var canvas = document.getElementById("tutorial");
 //    var ctx = canvas.getContext("2d");
