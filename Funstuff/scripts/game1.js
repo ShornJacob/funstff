52cd80d4cc24291ef318f8f875f95d00ad80235a
Shorn Jacob <shorn.jacob1986@gmail.com>
2016-11-14 17:21:55 +1300

    get-1

--- Funstuff/scripts/game1.js
+++ Funstuff/scripts/game1.js
@@ -15,9 +15,13 @@
 
     $("#but2").click(function () {
        
-        $("#div1").fadeTo("slow",0.15);
-        $("#div2").fadeTo("slow", 0.4);
-        $("#div3").fadeTo("slow", 0.7);
+        $("#element1").stop(true,false);
+    });
+
+    $("#but3").click(function () {
+
+        alert($("#but2").text());
+        alert($("#but2").html());
     });
 
     $("p").on({
