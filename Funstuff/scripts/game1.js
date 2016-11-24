b92ecbc973f5aaa4867f6dc2b63ddee61fb49d81
Shorn Jacob <shorn.jacob1986@gmail.com>
2016-11-24 16:06:32 +1300

    angular speed increase cube

--- Funstuff/scripts/game1.js
+++ Funstuff/scripts/game1.js
@@ -1,5 +1,7 @@
 ﻿"use strict";
 
+
+
 var gameCube;
 
 $(document).ready(function () {
@@ -8,18 +10,47 @@ $(document).ready(function () {
     gameCube = new Component(30, 30, "red", 10, 120);
     gameArea.start(gameCube);
 
+    $(document).keydown(function (myEvent) {
+        switch (myEvent.which) {
+            case 37: //left
+                moveLeft();
+                break;
+
+            case 38: //up
+                moveUp();
+                break;
+
+            case 39: //right
+                moveRight();
+                break;
+
+
+            case 40: //down
+                moveDown();
+                break;
+
+            default: return; //exit this handler for other keys
+        }
+        myEvent.preventDefault(); // prevent the default action 
+
+    });
+
+
+
 });
+
+
 
 //object initializer. Literal notation . This creates an object myGameArea with properties
 var gameArea = {
-   
+
 
     start: function (gameCube) {
         this.canvasWidth = $("#canvas1").attr("width");
         this.canvasHeight = $("#canvas1").attr("height");
         this.context = $("#canvas1")[0].getContext("2d");
         //this.gameCube = gameCube
-        this.refreshCanvas();       
+        this.refreshCanvas();
     },
 
     clear: function () {
@@ -28,12 +59,12 @@ var gameArea = {
     },
 
     refreshCanvas: function () {
-       
+
         setInterval(updateCanvas, 20);
 
     }
 
-   
+
 
 }
 
@@ -45,32 +76,90 @@ function Component(width, height, color, x, y) {
     this.width = width;
     this.height = height;
     this.color = color;
+    this.posX = x;
+    this.posY = y;
     this.x = x;
     this.y = y;
+    this.speed = 5;
 
     this.update = function () {
         //grab context from gameArea Object
         this.ctx = gameArea.context;
 
         this.ctx.fillStyle = this.color;
-        this.ctx.fillRect(this.x, this.y, this.width, this.height);
+        //parameters , x,y,width , height
+        //x	the x coordinate of the upper-left corner
+        //same as x and y when game starts
+        this.ctx.fillRect(this.posX, this.posY, this.width, this.height);
     }
+
+    //this.newPos = function () {
+    //    this.x += this.posX;
+    //    this.y += this.posY;
+    //}
 }
 
 
 function updateCanvas() {
-    console.log(new Date().getMilliseconds());
+    //console.log(new Date().getMilliseconds());
     //clear before update. updates last 20 ms
     gameArea.clear();
     gameCube.update();
-    gameCube.x += 1;
+
+
+}
+
+//functions to increment x and y coordinates
+//y is distance from top left. less distance, the less its further
+function moveUp() {
+
+    if (gameCube.posY != 0) {
+        gameCube.posY -= gameCube.speed;
+    }
+
+}
+
+
+function moveDown() {
+    if (gameCube.posY != (gameArea.canvasHeight - gameCube.height) ){
+        gameCube.posY += gameCube.speed;
+    } 
+}
+
+function moveLeft() {
+    if (gameCube.posX != 0) {
+        gameCube.posX -= gameCube.speed;
+    }
     
 }
+
+function moveRight() {
+    if (gameCube.posX != (gameArea.canvasWidth - gameCube.width)) {
+        gameCube.posX += gameCube.speed;
+    }
+    
+}
+
+
 //myGameArea.clear = function () {
 
 //    clear a rectangle within a given rectangle . this clears the whole canvas
 //    aContext.clearRect(0, 0, canvasWidth, canvasHeight);
 //}
 
+//Angular Part
 
+var app = angular.module('myApp', []);
 
+app.controller('myCtrl', function ($scope) {
+    $scope.cubeSpeedOptions = [
+            { value: 1, name: 'Slow' },
+            { value: 3, name: 'Medium' },
+            { value: 8, name: 'Fast' },
+    ];
+
+    $scope.cubeSpeedChange = function () {
+        gameCube.speed = $scope.selectedCubeSpeed['value'];
+    }
+
+});
