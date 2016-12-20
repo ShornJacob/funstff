0fa88c2ef3a5025a7ce02f01644b55f6ff27c005
Shorn Jacob <shorn.jacob1986@gmail.com>
2016-12-20 18:25:19 +1300

    snake 1

---
+++ Funstuff/scripts/game2/script_jquery.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+
+    //Canvas stuff
+    
+    //get the dom object from the jquery object
+    var canvas = $("#canvas")[0];
+    var ctx = canvas.getContext("2d");
+
+    //access the properties of element from jquery object
+    var w = $("#canvas").width();
+    var h = $("#canvas").height();
+
+    //cell width 
+    var cw = 10;
+    var d;
+    var food;
+    var score;
+
+
+    var snake_array; //an array of cells to make up the snake
+})
